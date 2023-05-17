910840dcb9b451264a5564e905676f35247606cd
balon14 <117996775+balon14@users.noreply.github.com>
2023-05-18 02:16:45 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+from transformers import YolosFeatureExtractor, YolosForObjectDetection
+from PIL import Image
+import requests
+import streamlit as st
+
+
+# Заголовок страницы
+st.title("Object detection YOLOS model Web App")
+
+# Получение имени пользователя и вывод приветствия
+name = st.text_input("Enter your name", "")
+st.write(f"Hello {name}!")
+
+
+url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+image = Image.open(requests.get(url, stream=True).raw)
+
+
+feature_extractor = YolosFeatureExtractor.from_pretrained('hustvl/yolos-tiny')
+model = YolosForObjectDetection.from_pretrained('hustvl/yolos-tiny')
+
+
+inputs = feature_extractor(images=image, return_tensors="pt")
+outputs = model(**inputs)
+
+
+# model predicts bounding boxes and corresponding COCO classes
+logits = outputs.logits
+bboxes = outputs.pred_boxes
